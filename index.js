c1008623971ef901cf72ceaf7397a0993fb95722
Adam Poit <metalkeys404@gmail.com>
2015-06-27 16:20:30 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,54 @@
+var express = require('express'),
+  bodyParser = require('body-parser'),
+  sh = require('shelljs'),
+  bunyan = require('bunyan');
+
+var repos = {
+  'git-node': '/home/adampoit/webapps/git_node/git_node'
+};
+
+var logger = bunyan.createLogger({
+  name: 'git-node',
+  streams: [
+    {
+      level: 'info',
+      path: '/home/adampoit/git_node_info.log'
+    },
+    {
+      level: 'error',
+      path: '/home/adampoit/git_node_error.log'
+    }
+  ]
+});
+
+var app = express();
+app.use(bodyParser.json());
+
+var server = app.listen(28596, function() {
+  logger.info('git-node started');
+});
+
+app.post('/', function(request, response, next) {
+  var repo = request.body.repository.name;
+  logger.info('Deploying ' + repo);
+
+  sh.cd(repos[repo]);
+  sh.exec('git pull', function (code, output) {
+    if (code !== 0)
+      return next(new Error(output));
+
+    sh.cd('..');
+    sh.exec('./deploy.sh', function (code, output) {
+      if (code !== 0)
+        return next(new Error(output));
+
+      response.sendStatus(200);
+    });
+  });
+});
+
+app.use(function (error, request, response, next) {
+  logger.error({ req: request, res: response, error: error }, error.stack);
+
+  response.sendStatus(500);
+});
