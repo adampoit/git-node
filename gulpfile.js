247adb270d2d25ba9a8fae66274d5ca1e95d28fb
Adam Poit <metalkeys404@gmail.com>
2015-07-02 10:30:21 -0700

    Add install task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp'),
+  install = require('gulp-install'),
   exec = require('child_process').exec;
 
-gulp.task('deploy', function (cb) {
+gulp.task('install', function () {
+  return gulp.src('./package.json')
+    .pipe(install());
+});
+
+gulp.task('deploy', ['install'], function (cb) {
   exec('../deploy.sh', function (err, stdout, stderr) {
     if (err !== null)
       cb(err);
