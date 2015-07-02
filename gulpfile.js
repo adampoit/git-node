48ad81eb31061592ccf930d0dd115908c6341fc8
Adam Poit <metalkeys404@gmail.com>
2015-07-02 11:42:08 -0700

    Use gulp-shell.

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,14 @@
 var gulp = require('gulp'),
   install = require('gulp-install'),
-  exec = require('child_process').exec;
+  shell = require('gulp-shell');
 
 gulp.task('install', function () {
   return gulp.src('./package.json')
     .pipe(install());
 });
 
-gulp.task('deploy', ['install'], function (cb) {
-  exec('../deploy.sh', function (err, stdout, stderr) {
-    if (err !== null)
-      cb(err);
-  });
-});
+gulp.task('deploy', ['install'], shell.task([
+  '../deploy.sh'
+]));
 
 gulp.task('default', ['deploy']);
